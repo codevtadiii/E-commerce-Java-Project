bc4097788028f93ede5e9a8fcec4ce1f8d981211
Aditya jha <medoremon0001@gmail.com>
2025-12-26 11:05:32 +0530

    Create MainCarousel.js

---
+++ ecom-java/src/customer/components/HomeCarousel/MainCarousel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { mainCarouselData } from './MainCarouselData';
+import 'react-alice-carousel/lib/alice-carousel.css';   
+import AliceCarousel from 'react-alice-carousel';
+
+const MainCarosel = () =>{
+
+    const items = mainCarouselData.map((item) => <img className='cursor-pointer' role='presentation' src={item.image} alt=""/>)
+
+    return(
+         <AliceCarousel
+        items={items}
+        disableButtonsControls
+        autoPlay
+        autoPlayInterval={1000}
+        infinite
+    />
+    )
+
+}
+
+export default MainCarosel;
