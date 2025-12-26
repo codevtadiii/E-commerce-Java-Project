38c3abc09ff75779fb0d8693af67ebf1233717c1
Aditya jha <medoremon0001@gmail.com>
2025-12-26 11:05:28 +0530

    Update App.js

--- ecom-java/src/App.js
+++ ecom-java/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
 import Navigation from "./customer/components/Navigation/Navigation.js";
+import HomePage from "./customer/pages/HomePage/HomePage.js";
+
+
 function App() {
   return (
-    <div className="App">
-     <Navigation/>
+    <div>
+      <div className=" ">
+        <Navigation/>
+      </div>
+      <div>
+        <HomePage/>
+      </div>
     </div>
   );
 }
