a67329237fe991fcdefc5e9dfad32a069878ccba
Aditya jha <medoremon0001@gmail.com>
2025-12-26 11:05:36 +0530

    Create MainCarouselData.js

---
+++ ecom-java/src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,22 @@
+export const mainCarouselData = [
+    {
+        image:
+            "https://www.ethnicplus.in/cdn/shop/files/2_99561f30-45b5-4401-aa04-9d979c61d32d.jpg?v=1762104598&width=2000",
+        path: "/women/clothing/lengha_choli",
+    },
+    {
+        image:
+            "https://www.ethnicplus.in/cdn/shop/files/11_bdfaaee4-3fb3-4a37-84c7-8d849172b556.jpg?v=1763662537&width=2000",
+        path: "/women/clothing/women_dress",
+    },
+    {
+        image:
+            "https://www.ethnicplus.in/cdn/shop/files/1_99fd8226-0466-4e7f-9914-64fcfaac5216.jpg?v=1762104598&width=2000",
+        path: "/women/clothing/women_dress",
+    },
+    {
+        image:
+            "https://www.ethnicplus.in/cdn/shop/files/3_ea3e9279-2114-4e9e-8d23-6085ce1a143c.jpg?v=1762104598&width=2000",
+        path: "/women/clothing/women_saree",
+    },
+];
