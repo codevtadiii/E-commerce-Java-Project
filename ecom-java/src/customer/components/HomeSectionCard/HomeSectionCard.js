e480e1cbc19f82c941a0e198d7a7b058b0e8b133
Aditya jha <medoremon0001@gmail.com>
2025-12-31 02:04:57 +0530

    Update HomeSectionCard.js

--- ecom-java/src/customer/components/HomeSectionCard/HomeSectionCard.js
+++ ecom-java/src/customer/components/HomeSectionCard/HomeSectionCard.js
@@ -2,16 +2,16 @@ import React from "react";
 import 'react-alice-carousel/lib/alice-carousel.css';  
 
 
-const HomeSectionCard = () => {
+const HomeSectionCard = ({product}) => {
     return (
-        <div className='cursor-pointer flex flex-col item-center bg-white rounded-lg shadow-overflow-hidden w-[15rem] mx-3'>
+        <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-overflow-hidden w-[15rem] mx-3 border '>
 
             <div className="h-[13rem] w-[10rem]">
-                <img className="object-cover object-top w-full h-full" src="https://www.ethnicplus.in/cdn/shop/files/61431_5_400x.jpg?v=1762694703" alt= ""/>
+                <img className="object-cover object-top w-full h-full" src={product.image} alt= ""/>
             </div>
             <div className="p-4">
-                <h3 className="text-lg font-medium text-gray-900">Men's Kurta</h3>
-                <p className="mt-2 text-sm text-gray-500">Men Solid lurta pure cotten</p>
+                <h3 className="text-lg font-medium text-gray-900">{product.brand}</h3>
+                <p className="mt-2 text-sm text-gray-500">{product.title}</p>
             </div>
             
         </div>
