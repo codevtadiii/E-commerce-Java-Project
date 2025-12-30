28c3168e3cc6ec3a72ae67c9776cdccd87956e00
Aditya jha <medoremon0001@gmail.com>
2025-12-31 02:05:01 +0530

    Update HomeSectionCarousel.js

--- ecom-java/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.js
+++ ecom-java/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import { Button } from  "@mui/material";
+import KeyboardArrowLeftIcon from '@mui/icons-material/ArrowLeft'; 
+import {mens_kurta} from "../../../Data/mens_kurta.js";
 
 const HopmeSectionCarousel = () =>{
+    const [activeIndex, setActiveIndex] =useState(0);
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
-        1024: { items: 5 },
+        1024: { items: 5.5 },
     };
 
-    const items = [1,1,1,1,1].map((item)=><HomeSectionCard/>)
+
+    const slidePrev = ()=>setActiveIndex(activeIndex-1);
+    const slideNext = ()=>setActiveIndex(activeIndex+1);
+
+    const syncActiveIndex = ({item})=>setActiveIndex(item);
+
+    const items =mens_kurta.slice(0,10).map((item)=><HomeSectionCard product={item}/>);
    
     return(
-        <div className="relative px-4 lg:px-8">
+        <div className="border ">
             <div className="relative p-5">
                 <AliceCarousel
                     items={items}
                     disableButtonsControls
                     infinite
                     responsive={responsive}
+                    disableDotsControls
+                    onSlideChange={syncActiveIndex}
+                    activeIndex={activeIndex}
                 />
-                <Button variant="content" className="z-50" sx={{position:'absolute', top:'8rem', right:"0rem", transform:"translate(50%) rotate(90deg)" , bgcolor:"white"}} aria-label="next">
-                    <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)"}}/>
+                {activeIndex !== items.length-5 &&<Button variant="content" className="z-50 bg-white"
+                onClick={slideNext} sx={{position:'absolute', top:'8rem', right:"0rem", transform:"translate(50%) rotate(90deg)" , bgcolor:"white"}} aria-label="next" >
+                <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)", color: "black"}}/>
+                </Button>}
+
+                <Button variant="content" className="z-50 bg-white"
+                onClick={slidePrev} sx={{position:'absolute', top:'8rem', left:"0rem", transform:"translate(50%) rotate(90deg)" , bgcolor:"white"}} aria-label="next">
+                <KeyboardArrowLeftIcon sx={{transform:"rotate(-90deg)", color: "black"}}/>
                 </Button>
             </div>
         </div>
